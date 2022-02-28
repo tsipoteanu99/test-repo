927cba18fa77212b20fbb8d367fa0681dd61d151
tsipoteanu <tsipoteanu99@gmail.com>
2022-02-28 02:02:07 +0200

    commit 2

---
+++ test1235/src/main/java/ASDF.java
@@ -0,0 +1,5 @@
+public class ASDF {
+    public void asdfgh(){
+        System.out.println("AFISEZ CV");
+    }
+}
--- test1235/src/main/java/Hello.java
+++ test1235/src/main/java/Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String args[]){
         System.out.println("Hello");
+
+
+
+        ASDF asdf = new ASDF();
+
+        asdf.asdfgh();
     }
 }
